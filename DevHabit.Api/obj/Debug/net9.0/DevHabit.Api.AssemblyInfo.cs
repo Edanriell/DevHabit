9b8707c3b8f5f95d515a4f2ed08c1a6557471a5b
Edanriell <Edanriell@gmail.com>
2025-05-29 16:31:56 +0300

    updated build.yml

--- DevHabit.Api/obj/Debug/net9.0/DevHabit.Api.AssemblyInfo.cs
+++ DevHabit.Api/obj/Debug/net9.0/DevHabit.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70582dc90bf9c50c7c68627083140322c91ee8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevHabit.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
