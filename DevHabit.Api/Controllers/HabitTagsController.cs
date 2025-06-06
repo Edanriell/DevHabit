73e4852d929c27fdcaee42d759275dfb384fa15d
Edanriell <Edanriell@gmail.com>
2025-06-06 15:08:13 +0300

    feature(child-resources2): created upsert habit tags dto

---
+++ DevHabit.Api/Controllers/HabitTagsController.cs
@@ -0,0 +1,19 @@
+﻿using DevHabit.Api.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevHabit.Api.Controllers;
+
+public sealed class HabitTagsController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpPut]
+    public async Task<ActionResult> UpsertHabitTags(string habitId, string[] tagIds)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{tagId}")]
+    public async Task<ActionResult> RemoveTagFromHabit(string id, string tagId)
+    {
+        return Ok();
+    }
+}
