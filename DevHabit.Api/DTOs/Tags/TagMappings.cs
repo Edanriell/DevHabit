23ded7466842b6474adaf4837082a49f63008159
Edanriell <Edanriell@gmail.com>
2025-06-06 13:33:17 +0300

    feature(tags): created tag mappings

---
+++ DevHabit.Api/DTOs/Tags/TagMappings.cs
@@ -0,0 +1,29 @@
+﻿namespace DevHabit.Api.DTOs.Tags;
+
+internal static class TagMappings
+{
+    public static TagDto ToDto(this Tag tag)
+    {
+        return new TagDto
+        {
+            Id = tag.Id,
+            Name = tag.Name,
+            Description = tag.Description,
+            CreatedAtUtc = tag.CreatedAtUtc,
+            UpdatedAtUtc = tag.UpdatedAtUtc
+        };
+    }
+
+    public static Tag ToEntity(this CreateTagDto dto)
+    {
+        Tag habit = new()
+        {
+            Id = $"t_{Guid.CreateVersion7()}",
+            Name = dto.Name,
+            Description = dto.Description,
+            CreatedAtUtc = DateTime.UtcNow
+        };
+
+        return habit;
+    }
+}
