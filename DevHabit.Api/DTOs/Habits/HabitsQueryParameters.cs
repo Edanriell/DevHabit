f32da78c60b7b4595ea69dcf0f2d9bd168c744e8
Edanriell <Edanriell@gmail.com>
2025-08-06 20:13:46 +0300

    feature(data-shaping): implemented the ability to shape data

--- DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
+++ DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
@@ -9,6 +9,7 @@ public sealed record HabitsQueryParameters
     public HabitType? Type { get; init; }
     public HabitStatus? Status { get; init; }
     public string? Sort { get; init; }
+    public string? Fields { get; init; }
     public int Page { get; init; }
     public int PageSize { get; init; } = 10;
 }
