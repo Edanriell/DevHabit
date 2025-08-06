1980efa280f1efb6b9cfedbf58e627ab9be872cf
Edanriell <Edanriell@gmail.com>
2025-08-06 17:38:45 +0300

    feature(sorting): implemented robust sorting

---
+++ DevHabit.Api/Services/Sorting/SortMappingDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace DevHabit.Api.Services.Sorting;
+
+#pragma warning disable S2326
+public sealed class SortMappingDefinition<TSource, TDestination> : ISortMappingDefinition
+#pragma warning restore S2326
+{
+    public required SortMapping[] Mappings { get; set; }
+}
