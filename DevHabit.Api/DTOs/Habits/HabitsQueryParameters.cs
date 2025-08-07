01180e5872959ba4cfdf80eb29ab64df68cde0bd
Edanriell <Edanriell@gmail.com>
2025-08-07 15:26:39 +0300

    feature(minor-refactor): separated some code

--- DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
+++ DevHabit.Api/DTOs/Habits/HabitsQueryParameters.cs
@@ -10,6 +10,6 @@ public sealed record HabitsQueryParameters
     public HabitStatus? Status { get; init; }
     public string? Sort { get; init; }
     public string? Fields { get; init; }
-    public int Page { get; init; }
+    public int Page { get; init; } = 1;
     public int PageSize { get; init; } = 10;
 }
